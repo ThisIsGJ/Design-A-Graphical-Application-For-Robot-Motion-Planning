e46e27a610178a648a8907133d338ef4e7f742bf
ThisIsGJ <guan_jun@live.cn>
2015-02-05 00:38:03 +0000

    finish basic set point
    finish basic set point

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -15,12 +15,21 @@ public class UserInterface implements ActionListener{
 	private DrawCanvas robotPanel;
 	private ArrayList<Point> pointClicked;
 	
+	private Boolean setStart;
+	private Boolean setEnd;
+	private Point startPoint;
+	private Point endPoint;
+	
 	public UserInterface(){
 		frame = new JFrame("Robot Motion");
 		contentPanel = frame.getContentPane(); 
 		buttonPanel = new JPanel();
 		robotPanel = new DrawCanvas();
 		pointClicked = new ArrayList<Point>();
+		setStart = false;
+		setEnd = false;
+		startPoint = new Point(0,0);
+		endPoint = new Point(0,0);
 		
 		MakeFrame();
 		
@@ -31,6 +40,8 @@ public class UserInterface implements ActionListener{
 		
 		//build buttonPanel
 		buttonPanel.setLayout(new GridLayout(15,1));
+		addButton(buttonPanel,"Start Point");
+		addButton(buttonPanel,"End Point");
 		addButton(buttonPanel,"Draw Shape");
 		addButton(buttonPanel,"Clean All");
 		addButton(buttonPanel,"Start");
@@ -58,8 +69,23 @@ public class UserInterface implements ActionListener{
 	
 	public void actionPerformed(ActionEvent e) {
 		String choice = e.getActionCommand();
-		if(choice == "Start"){
-			System.out.println("Start");
+		if(choice == "Draw Shape"){
+			System.out.println("Drawed!!");
+		}else if(choice == "Start Point"){
+			setStart = true;
+			setEnd = false;
+		}else if(choice == "End Point"){
+			setEnd = true;
+			setStart = false;
+		}else if(choice == "Clean All"){
+			setStart = false;
+			setEnd = false;
+			startPoint.x = 0;
+			startPoint.y = 0;
+			endPoint.y = 0;
+			endPoint.x = 0;
+			pointClicked.clear();
+			robotPanel.repaint();
 		}
 	}
 	
@@ -69,7 +95,17 @@ public class UserInterface implements ActionListener{
 		public DrawCanvas(){
 			this.addMouseListener(new MouseAdapter() {
 	             public void mouseClicked(MouseEvent e) {
-	                 pointClicked.add(e.getPoint());
+	            	 
+	            	 if(setStart == true){
+	    	        	 startPoint.x = e.getPoint().x;
+	    	        	 startPoint.y = e.getPoint().y;
+	    	         }else if(setEnd == true){
+	    	        	 endPoint.x = e.getPoint().x;
+	    	        	 endPoint.y = e.getPoint().y;
+	    	         }else{
+	    	        	 pointClicked.add(e.getPoint());
+	    	         }    
+	            	 
 	                 repaint();
 	             }
 	         });	
@@ -78,14 +114,29 @@ public class UserInterface implements ActionListener{
 		@Override
 	      public void paintComponent(Graphics g) {
 	         super.paintComponent(g);
+
+	         //draw start point
+	         if(startPoint.x != 0 || startPoint.y != 0 ){
+	        	 g.setColor(Color.RED);
+	        	 g.fillRect(startPoint.x,startPoint.y,10,10);
+	        	 setStart = false;
+	         }
+	         
+	         //draw end point
+	         if(endPoint.x != 0 || endPoint.y != 0){
+	        	 g.setColor(Color.BLUE);
+	        	 g.fillRect(endPoint.x, endPoint.y, 10, 10);
+	        	 setEnd = false;
+	         }
+	         
 	         if(pointClicked.size() != 0){
+	        	 g.setColor(Color.BLACK);
 	        	 for (int i = 0; i < pointClicked.size(); i++){
 	        		 g.fillRect(pointClicked.get(i).x, pointClicked.get(i).y, 5, 5);
 	        	 }    
 	         }
 	      }	
 	}
-	
 	
 }
 
